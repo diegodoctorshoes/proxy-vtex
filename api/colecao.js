09cd936e823cea3a68aa95ac5cb85cc530665be2
diegodoctorshoes <diego@doctorshoes.com.br>
2025-10-06 11:16:31 -0300

    Alterando ordenacao da lista

--- api/colecao.js
+++ api/colecao.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      `https://doctorshoes.vtexcommercestable.com.br/api/catalog_system/pub/products/search?fq=H:${id}`
+      `https://doctorshoes.vtexcommercestable.com.br/api/catalog_system/pub/products/search?fq=H:${id}&O=OrderByTopSaleDESC`
     );
 
     if (!response.ok) {
